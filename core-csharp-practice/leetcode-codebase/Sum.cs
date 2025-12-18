201628e0cda695a5e9b4713f16a252942867d305
practice <aditi.pathak_cs22@gla.ac.in>
2025-12-18 15:23:55 +0530

    [Aditi Pathak] first commit

---
+++ core-csharp-practice/leetcode-codebase/Sum.cs
@@ -0,0 +1,15 @@
+using System;
+class Sum
+{
+	static void Main()
+	{
+		
+		Console.Write("Enter a");
+		int a=int.Parse(Console.ReadLine());
+		Console.Write("Enter b");
+		int b=int.Parse(Console.ReadLine());
+		int sum=a+b;
+		Console.WriteLine(sum);
+	}
+}
+		
