f36744d3d69bb8f7e018b076cc50a72db2d950a2
practice <aditi.pathak_cs22@gla.ac.in>
2025-12-19 12:31:25 +0530

    [Aditi Pathak] final commit]

---
+++ core-csharp-practice/gcr codebase/programming-elements/DataType.cs
@@ -0,0 +1,36 @@
+ //datatype
+ using System;
+ class DataType
+ {
+	 static void Main()
+	 {
+		 //Integer
+		 int a=5;
+		 byte b=255;
+		 short sh=32000;
+		 long l=124567788;
+		 //float
+		 float f=10.3f;
+		 double d=28.23;
+		 decimal dc=1234.45m;
+		 //character
+		 char c='A';
+		 String s="Aditi";
+		 //boolean
+		// bool isValid=true;
+		 Console.WriteLine(a);
+		 Console.WriteLine(b);
+		 Console.WriteLine(sh);
+		 Console.WriteLine(l);
+		 Console.WriteLine(f);
+		 Console.WriteLine(d);
+		 Console.WriteLine(dc);
+		 Console.WriteLine(c);
+		 Console.WriteLine(s);
+	 }
+ }
+		 
+		 
+	
+		 
+		 
